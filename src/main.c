6f80dfdde393ace8205f545e359c93ff258fab49
Brian Hong <brian@hongs.me>
2017-05-18 16:12:47 -0400

    Finished init

--- src/main.c
+++ src/main.c
@@ -20,6 +20,7 @@ int main(int argc, char **argv){
 		if(argc < 3) print_usage(argv[0]);
 		db_init("init", argv+2);
 	}else if(!strcmp(argv[1], "import")){
+		if(argc < 3) print_usage(argv[0]);
 		db_import("import", argv+2);
 	}else{
 		printf("Unknown command \'%s\'. Aborting.\n", argv[1]);
