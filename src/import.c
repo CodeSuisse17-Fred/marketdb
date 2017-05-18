56242958e9aeefb35db708d997a70ed2969743e0
Brian Hong <brian@hongs.me>
2017-05-18 18:07:57 -0400

    Finished import from file?

--- src/import.c
+++ src/import.c
@@ -1,3 +1,84 @@
-int db_import(char *fn, char **argv){
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <errno.h>
 
+int db_import(char *fn, char **argv){
+	printf("Starting import from file...\n");
+	char *db = argv[0];
+	char *inf = argv[1];
+	printf("Opening file %s\n", inf);
+	FILE *fp = fopen(inf, "r");
+
+	struct stat st = {0};
+
+	while(fgetc(fp) != '\n');
+	while(!feof(fp)){
+		char *tickr;
+		char *date;
+		int ms;
+		float price;
+
+		char *line = malloc(64);
+		char *lp = line;
+		fscanf(fp, "%s\n", line);
+
+		// Tokenize parts
+		tickr = strsep(&line, ",");
+		date = strsep(&line, ".");
+		ms = atoi(strsep(&line, ","));
+		strsep(&line, ",");
+		price = strtof(strsep(&line, ","), NULL);
+
+		char fname1[20], fname2[20];
+		sprintf(fname1, "%s_%.8s_%s", tickr, date, date+8);
+		printf("%s\n", fname1);
+
+		char path[40] = {0};
+		strcat(path, db);
+		strcat(path, "/unsorted/");
+		strcat(path, fname1);
+		FILE *fp2 = fopen(path, "a");
+		fprintf(fp2, "%d %f\n", ms, price);
+		fclose(fp2);
+
+		char path2[40] = {0};
+		strcat(path2, db);
+		strcat(path2, "/by-tikr/");
+		strcat(path2, tickr);
+
+	//	printf("%s\n", path2);
+		if(stat(path2, &st) || !S_ISDIR(st.st_mode))
+			if(mkdir(path2, 0755))
+				fprintf(stderr, "Error:%s\n", strerror(errno));
+
+		strcat(path2, "/");
+		sprintf(fname2, "%s", date+8);
+		strcat(path2, fname2);
+
+		link(path, path2);
+
+		char path3[40] = {0};
+		strcat(path3, db);
+		strcat(path3, "/by-date/");
+		char *t = strdup(date);
+		t[8] = 0;
+		strcat(path3, t);
+		free(t);
+
+	//	printf("%s\n", path3);
+		if(stat(path3, &st) || !S_ISDIR(st.st_mode))
+			if(mkdir(path3, 0755))
+				fprintf(stderr, "Error:%s\n", strerror(errno));
+		
+		strcat(path3, "/");
+		strcat(path3, fname2);
+		link(path, path3);
+
+		free(lp);
+	}
+
+	fclose(fp);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 char *usage = "Usage:\n%s [COMMAND] <PARAMETERS>\n\
   Commands list:\n\
     init\t[dir]\n\
-    import\t[filename]\n";
+    import\t[dir] [filename]\n";
 
 void print_usage(char *pn){
 	printf(usage, pn);
@@ -20,7 +20,7 @@ int main(int argc, char **argv){
 		if(argc < 3) print_usage(argv[0]);
 		db_init("init", argv+2);
 	}else if(!strcmp(argv[1], "import")){
-		if(argc < 3) print_usage(argv[0]);
+		if(argc < 4) print_usage(argv[0]);
 		db_import("import", argv+2);
 	}else{
 		printf("Unknown command \'%s\'. Aborting.\n", argv[1]);
